55a93a5eedf9d5d378907cbe8a98b77afd785533
moe <moe9999@protonmail.com>
2020-03-29 22:18:49 +0100

    Initial build

--- js/index.js
+++ js/index.js
@@ -3,16 +3,7 @@
 $(document).ready(function () {
   var converter = new showdown.Converter();
 
-  var switches = {
-    title: false,
-    url: false,
-    icon: false,
-    useVars: false
-  };
-
-  var fields = 1;
-
-  var source = '';
+  var fields = 0;
 
   var embed = {
     title: '',
@@ -23,10 +14,15 @@ $(document).ready(function () {
     },
     description: '',
     url: '',
-    thumb_url: '',
+    thumbnail: {
+      url: ''
+    },
     color: '',
-    fields: [{}],
-    footer: ''
+    fields: [],
+    footer: {
+      text: '',
+      iconUrl: ''
+    }
   };
 
   function resetEmbed() {
@@ -38,163 +34,52 @@ $(document).ready(function () {
   function updateEmbed(embed) {
     resetEmbed();
 
-    // add basic embed generation to source
-    source = 'embed=discord.Embed(';
-
     if (embed.url) {
       $('.embed-inner').append('<div class="embed-title"><a href="' + embed.url + '">' + embed.title + '</a></div>');
-
-      // update source
-      if (switches.useVars) {
-        source += 'title=' + embed.title + ', url=' + embed.url;
-      } else {
-        source += 'title="' + embed.title + '", url="' + embed.url + '"';
-      }
-    } else if (embed.title.length === 0) {
-      source += "";
     } else {
       $('.embed-inner').append('<div class="embed-title">' + embed.title + '</div>');
-
-      // update source
-      if (switches.useVars) {
-        source += 'title=' + embed.title;
-      } else {
-        source += 'title="' + embed.title + '"';
-      }
-
     }
 
     if (embed.description) {
       $('.embed-inner').append('<div class="embed-description">' + converter.makeHtml(embed.description) + '</div>');
-
-      if (embed.title.length > 0 || embed.url.length > 0) {
-        source += ', '
-      }
-
-      // update source
-      if (switches.useVars) {
-        source += 'description=' + embed.description;
-      } else {
-        source += 'description="' + embed.description + '"';
-      }
     }
 
     if (embed.color) {
-      $('.side-colored').css('background-color', embed.color);
-
-      if (embed.title.length > 0 || embed.url.length > 0) {
-        source += ', '
-      }
-
-      // update source
-      source += 'color=0x' + embed.color.substr(1);
+      $('.side-colored').css('background-color', '#' + embed.color.toString(16));
     }
 
-    // finished the basic setup
-    source += ')\n';
-
     if (embed.author.name) {
-      // add author to source
-      source += 'embed.set_author(';
-
       $('.embed-title').before('<div class="embed-author"><a class="embed-author-name" href="' + embed.author.url + '">' + embed.author.name + '</a></div>');
-
-      // update source
-      if (switches.useVars) {
-        source += 'name=' + embed.author.name;
-      } else {
-        source += 'name="' + embed.author.name + '"';
-      }
-
-      if (embed.author.url) {
-        source += ', ';
-
-        if (switches.useVars) {
-          source += 'url=' + embed.author.url;
-        } else {
-          source += 'url="' + embed.author.url + '"';
-        }
-      }
 
       if (embed.author.icon) {
         $('.embed-author-name').before('<img class="embed-author-icon" src="' + embed.author.icon + '" />');
-
-        source += ',';
-
-        // update source
-        if (switches.useVars) {
-          source += 'icon_url=' + embed.author.icon;
-        } else {
-          source += ', icon_url="' + embed.author.icon + '"';
-        }
       }
-
-      // finish author
-      source += ')\n';
     }
 
-    if (embed.thumb_url) {
-      // add thumbnail
-      source += 'embed.set_thumbnail(';
-
-      $('.card.embed .card-block').append('<img class="embed-thumb" src="' + embed.thumb_url + '" />');
+    if (embed.thumbnail.url) {
+      $('.card.embed .card-block').append('<img class="embed-thumb" src="' + embed.thumbnail.url + '" />');
       $('.embed-thumb').height($('.embed-thumb')[0].naturalHeight);
-
-      // update source
-      if (switches.useVars) {
-        source += 'url=' + embed.thumb_url;
-      } else {
-        source += 'url="' + embed.thumb_url + '"';
-      }
-
-      // finish thumbnail
-      source += ')\n';
     }
 
     if (embed.fields.length > 0) {
       $('.embed-inner').append('<div class="fields"></div>');
     }
 
-    for (var _iterator = embed.fields, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
-      var _ref;
+    for (var i = 0; i < embed.fields.length; i++) {
+      var field = embed.fields[i];
+      var name = field.name ? field.name : '';
+      var value = field.value ? field.value : '';
+      $('.embed-inner .fields').append('\n        <div class="field ' + (field.inline && 'inline') + '">\n          <div class="field-name">' + name + '</div>\n          <div class="field-value">' + converter.makeHtml(value) + '</div>\n        </div>\n      ');
+    }
 
-      if (_isArray) {
-        if (_i >= _iterator.length) break;
-        _ref = _iterator[_i++];
-      } else {
-        _i = _iterator.next();
-        if (_i.done) break;
-        _ref = _i.value;
-      }
-
-      var field = _ref;
-
-      $('.embed-inner .fields').append('\n        <div class="field ' + (field.inline && 'inline') + '">\n          <div class="field-name">' + field.name + '</div>\n          <div class="field-value">' + converter.makeHtml(field.value) + '</div>\n        </div>\n      ');
-
-      // add field
-      if (switches.useVars) {
-        source += 'embed.add_field(name=' + field.name + ', value=' + field.value + ', inline=' + (field.inline && 'True' || 'False') + ')\n';
-      } else {
-        source += 'embed.add_field(name="' + field.name + '", value="' + field.value + '", inline=' + (field.inline && 'True' || 'False') + ')\n';
+    if (embed.footer.text) {
+      $('.card.embed').append('<div class="embed-footer"><span>' + embed.footer.text + '</span></div>');
+      if (embed.footer.iconUrl) {
+        $('.embed-footer span').before('<img class="embed-footer-icon" src="' + embed.footer.iconUrl + '" />');
       }
     }
 
-    if (embed.footer) {
-      $('.card.embed').append('<div class="embed-footer"><span>' + embed.footer + '</span></div>');
-
-      // add footer
-      if (switches.useVars) {
-        source += 'embed.set_footer(text=' + embed.footer + ')\n';
-      } else {
-        source += 'embed.set_footer(text="' + embed.footer + '")\n';
-      }
-    }
-
-    // add send function
-    source += 'await self.bot.say(embed=embed)\n';
-
-    // code
-    $('.source').text(source);
+    $('.source').text(JSON.stringify(embed, null, " "));
     hljs.highlightBlock($('.source')[0]);
   }
 
@@ -278,7 +163,7 @@ $(document).ready(function () {
   }
 
   function updateThumb(value) {
-    embed.thumb_url = value || false;
+    embed.thumbnail.url = value || false;
     updateEmbed(embed);
   }
 
@@ -288,7 +173,12 @@ $(document).ready(function () {
   }
 
   function updateColor(value) {
-    embed.color = value || false;
+    if (value) {
+      value = value.substr(1);
+      embed.color = parseInt(value, 16);
+    } else {
+      embed.color = '';
+    }
     updateEmbed(embed);
   }
 
@@ -307,8 +197,13 @@ $(document).ready(function () {
     updateEmbed(embed);
   }
 
-  function updateFooter(value) {
-    embed.footer = value || '';
+  function updateFooterText(value) {
+    embed.footer.text = value || '';
+    updateEmbed(embed);
+  }
+
+  function updateFooterUrl(value) {
+    embed.footer.iconUrl = value || '';
     updateEmbed(embed);
   }
 
@@ -347,7 +242,7 @@ $(document).ready(function () {
     var item = $('#url');
     var url = item.val();
 
-    if (url.substr(0, 4) !== 'http' && url.length !== 0 && !switches.useVars) {
+    if (url.substr(0, 4) !== 'http' && url.length !== 0) {
       addWarning(item, 'url', 'not a valid url');
     } else {
       addSuccess(item, 'url');
@@ -360,7 +255,7 @@ $(document).ready(function () {
     var item = $('#icon');
     var icon = item.val();
 
-    if (icon.substr(0, 4) !== 'http' && icon.length !== 0 && !switches.useVars) {
+    if (icon.substr(0, 4) !== 'http' && icon.length !== 0) {
       addWarning(item, 'icon', 'not a valid url');
     } else {
       addSuccess(item, 'icon');
@@ -394,7 +289,7 @@ $(document).ready(function () {
     var item = $('#author_url');
     var author_url = item.val();
 
-    if (author_url.substr(0, 4) !== 'http' && author_url.length !== 0 && !switches.useVars) {
+    if (author_url.substr(0, 4) !== 'http' && author_url.length !== 0) {
       addWarning(item, 'author_url', 'not a valid url');
     } else {
       addSuccess(item, 'author_url');
@@ -407,7 +302,7 @@ $(document).ready(function () {
     var item = $('#author_icon');
     var author_icon = item.val();
 
-    if (author_icon.substr(0, 4) !== 'http' && author_icon.length !== 0 && !switches.useVars) {
+    if (author_icon.substr(0, 4) !== 'http' && author_icon.length !== 0) {
       addWarning(item, 'author_icon', 'not a valid url');
     } else {
       addSuccess(item, 'author_icon');
@@ -416,17 +311,25 @@ $(document).ready(function () {
     }
   });
 
-  $('#footer').keyup(function () {
-    var item = $('#footer');
-    var footer = item.val();
+  $('#footer_name').keyup(function () {
+    var item = $('#footer_name');
+    var footer_name = item.val();
 
-    addSuccess(item, 'footer');
+    addSuccess(item, 'footer_name');
     // update
-    updateFooter(footer);
+    updateFooterText(footer_name);
   });
 
-  $('#useVars').click(function () {
-    switches.useVars = !switches.useVars;
-    updateEmbed(embed);
+  $('#footer_url').keyup(function () {
+    var item = $('#footer_url');
+    var footer_url = item.val();
+
+    if (footer_url.substr(0, 4) !== 'http' && footer_url.length !== 0) {
+      addWarning(item, 'footer_url', 'not a valid url');
+    } else {
+      addSuccess(item, 'footer_url');
+      // update
+      updateFooterUrl(footer_url);
+    }
   });
 });
